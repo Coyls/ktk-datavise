655ee4932c92a1c79d7af8e3741e2a1e53f1820d
Coyls <loic.grecofr@gmail.com>
2021-12-05 23:51:48 +0100

    refactor(dist): release

---
+++ dist/index.c5c4f12f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#knowledge .wrapper"),r=document.querySelector("#knowledge .arrowWrapper .previousSlide"),o=document.querySelector("#knowledge .arrowWrapper .nextSlide");new Swiper(e,{direction:"horizontal",effect:"cards",loop:!0,slideShadows:!1,speed:300,autoplay:{delay:5e3,disableOnInteraction:!1},navigation:{nextEl:o,prevEl:r}});
+//# sourceMappingURL=index.c5c4f12f.js.map
